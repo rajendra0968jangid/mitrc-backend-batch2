74116ec870e98f1e3cd738ce2ada0c809259be10
rajendra0968jangid <rajendra0968jangid@gmail.com>
2025-05-02 14:41:25 +0530

    Initial commit

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+            <div className='container'>
+                <div className='row'>
+                    <div className=''>
+                        <div className="card" style={{ width: "18rem" }}>
+                            <img src="..." className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h5 className="card-title">Price</h5>
+                                <p className="card-text">
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </>
+    )
+}
+
+export default Home
